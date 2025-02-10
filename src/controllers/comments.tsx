bcb4beadc7f280e98abd810d5d77201c9fa9d17b
Billy Pamanian <itisbilly@gmail.com>
2025-02-10 02:38:56 -0800

    Created POST method logic for posts/comments endpoint

--- src/controllers/comments.tsx
+++ src/controllers/comments.tsx
@@ -6,11 +6,40 @@ import { turso } from "../library/dev_turso.js";
 async function createComment(c: Context) {
   let status: ContentfulStatusCode = 500;
   const userUUID = c.get('uuid');
+  const postUUID = c.req.param('postId');
+  const content = c.req.queries('content');
   const response: APIResponse = {
     success: String(status).search("2") === 0 ? true : false,
     path: `${c.req.path}`,
-    message: 'POST not yet implemented',
+    message: 'Server error: POST comment',
   }
+
+  const transaction = await turso.transaction();
+
+  try {
+    const postComment = await transaction.execute({
+      sql: `INSERT INTO comments (user_id, post_id, content)
+        VALUES (
+          (SELECT id FROM users WHERE uuid = ?),
+          (SELECT id FROM posts WHERE uuid = ?),
+          ?
+        );
+      `,
+      args: [userUUID, postUUID, content],
+    });
+
+    if (postComment.rowsAffected != 0) {
+      status = 200;
+      response.message = "Comment posted";
+      response.data = [...postComment.rows];
+      await transaction.commit();
+    }
+  } catch (err) {
+    response.message = `Database error: POST comment: ${err}`;
+  } finally {
+    transaction.close();
+  }
+
   response.success = String(status).search("2") === 0 ? true : false;
   return c.json(response, status);
 }
@@ -22,7 +51,29 @@ async function readCommentDetail(c: Context) {
   const response: APIResponse = {
     success: String(status).search("2") === 0 ? true : false,
     path: `${c.req.path}`,
-    message: 'POST not yet implemented',
+    message: 'Server error: GET comments detail',
+  }
+
+  try {
+    const commentDetail = await turso.execute({
+      sql: `SELECT u.uuid as user_uuid, c.uuid as comment_uuid, p.uuid as post_uuid,
+        u.handle, u.display_name, u.avatar, c.content, c.created_at
+        FROM users u
+        JOIN comments c ON c.user_id = u.id
+        JOIN posts p ON p.id = c.post_id
+        WHERE c.uuid = ?;
+      `,
+      args: [commentUUID],
+    });
+
+    if (commentDetail.rows.length != 0) {
+      status = 200;
+      response.message = "Comment retrieved successfully";
+      response.data = [...commentDetail.rows];
+    }
+  } catch (err) {
+    console.error(err);
+    response.message = `Could not retrieve comment: ${err}`
   }
 
   response.success = String(status).search("2") === 0 ? true : false;
